46b7e3bb66b57522ca1e56bfca21d4d45f2edbe1
warmachineasce <151731942+warmachineasce@users.noreply.github.com>
2024-01-14 00:19:49 +0530

    Update Try.py

--- Try.py
+++ Try.py
@@ -6,18 +6,21 @@ import random
 from telethon import TelegramClient, events
 from telethon.sessions import StringSession
 
-api_id = 'your_api_id'
-api_hash = 'your_api_hash'
+api_id = '27668783'
+api_hash = '0931ae1c0f547465580a2e563e5e1bfa'
 
-session_string = 'your_session_string'
-username = 'your_username'
+session_string = '1BVtsOGkBu6QFBhOuwd4pgrjqDdpDJzS_dRkgAvBgR7MtYOwuUjoJ4BR44oqP0su_QgLVzH6ADJL_W5mCRsUPF4a6wqiZae8VMwoClmKNFsGqdWbInlkOd9uDnkPmBebQNqSENW8j4EApg8vFlRTrAlio9mHJ391AgnZR-M9XUMpf6w0R83Xwiy9rgC1UglC6TQOWIwFFz108torAE_RXcpc4US4Yv8Fgj9eMr-tQWtUOIb68FAKZaUpgPRR_jC8QybCvmlQAU6T0gHsPhQF18tMKGE_nhPAEn8YdNS5ySjSkhRt_j-ljZhQunHvYOG61wxFtHtuH85DYIe1BvIudY-h_n16yO00='
+username = '@roronoa_zoro_robot'
 
 session = StringSession(session_string)
 client = TelegramClient(session, api_id, api_hash)
 
+async def run_bot():
+    await client.start()
+    await main()
+    await client.run_until_disconnected()
 
-@client.on(events.NewMessage(from_users=[username]))
-async def _(event):
+async def main():
     # Send the /bal command
     await client.send_message(username, "/bal")
 
@@ -33,9 +36,6 @@ async def _(event):
         # Divide the bounty dynamically based on the current amount
         divided_bounties = [current_bounty // (2 ** i) for i in range(4)]
 
-        # Wait for 1 second before placing the first bet
-        await asyncio.sleep(1)
-
         # Loop indefinitely and place /bet commands with a 4-second delay
         while True:
             for bet_amount in divided_bounties:
@@ -46,16 +46,5 @@ async def _(event):
                 # Wait for 4 seconds before placing the next bet
                 await asyncio.sleep(4)
 
-    # Disconnect and restart the script
-    await client.disconnect()
-    os.execl(sys.executable, sys.executable, *sys.argv)
-
-
-async def main():
-    await client.start()
-    await client.run_until_disconnected()
-
-
-loop = asyncio.get_event_loop()
-loop.run_until_complete(main())
-  
+# Run the bot in a separate thread
+client_thread = asyncio.new_event_loop().create_task(run_bot())
